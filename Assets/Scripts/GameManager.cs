ee898f53c54aa579e66b697e96cbd4dc978a6aef
정진오 <jino9492@gmail.com>
2021-10-15 00:04:06 +0900

    손전등 업그레이드
    인게임에서 F를 누르면 불이 켜지므로 참고

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject Fog;
+    public GameObject globalLight;
 
     private void Start()
     {
-        Fog.SetActive(true);
+        globalLight.SetActive(false);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F)) // F를 눌러 불 좀 켜줄래?
-            Fog.SetActive(!Fog.activeSelf);
+            globalLight.SetActive(!globalLight.activeSelf);
     }
 }
