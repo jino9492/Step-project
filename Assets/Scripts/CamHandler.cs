39b4d5f6e3e92a2513fa46890c954d88fc03065e
정진오 <jino9492@gmail.com>
2021-10-14 19:38:00 +0900

    플레이어랑 카메라 움직임 구현

---
+++ Assets/Scripts/CamHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamHandler : MonoBehaviour
+{
+    public Transform camTransform;
+    public Transform playerTransform;
+    public Vector3 camOffset;
+
+    private void Start()
+    {
+        camOffset = new Vector3(0, 0, camTransform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        camTransform.position = Vector3.Lerp(camTransform.position, playerTransform.position + camOffset, 0.5f);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public Vector2 moveInput;
+
+
+    public void Update()
     {
-        
+        #region Move 
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput = moveInput.normalized;
+        #endregion
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 }
