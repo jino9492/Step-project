d3bacf7fe47014b3f4c164edfa7820c4342b5656
정진오 <jino9492@gmail.com>
2021-12-24 11:55:59 +0900

    메인화면 1

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
